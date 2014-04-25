cbf154002021afb786fcdf0111214b7caf3c9580
Philip Morkisch <philip.morkisch@brandung.de>
2014-04-25 15:11:44 +0200

    - updated loadmodule, added basketjs as standalone module, removed jquery-loading from modules.html, moved main.js reference in modules.html to the head, loading jquery and loadingmodule via basket.js, updated prefix mixin to support transform in ie, removed reference to jquery.mapfile which caused 404

--- js/global.js
+++ js/global.js
@@ -16,7 +16,7 @@ if (!(window.console && console.log)) {
 /**
 #####################################
 #                                   #
-#         Project Namespace         #
+#    Project Namespace              #
 #                                   #
 #####################################
 */
@@ -34,152 +34,181 @@ Brandung.folderPath = '%%public%%/js';
  */
 Brandung.IE8 = (document.all && !document.addEventListener) ? true : false;
 
-
-/**
- #####################################
- #                                   #
- #    mobile stuff functions         #
- #                                   #
- #####################################
- */
-Brandung.Mobile = {
-
-	init : function() {
-	}
-};
-
-/**
- * check orientation
- * 
- * add orientation class to body tag
- */
-Brandung.Mobile.checkOrientation = function() {
-
-	if (window.orientation != 0) {
-		$('body').removeClass('portrait').addClass('landscape');
-	} else {
-		$('body').removeClass('landscape').addClass('portrait');
-	}
-	
-};
-
-/**
- * get media device selector
- */
-
-if (Brandung.IE8) {
-	Brandung.Mobile.mediaDevice = 'desktop';
-} else {
-	Brandung.Mobile.mediaDevice = window.getComputedStyle(document.body, ':after').getPropertyValue('content');	
-}
-
-Brandung.Mobile.getWindowWidth = function() {
-	if (Brandung.IE8) {  // feature detection for IE <= IE8
-		Brandung.Mobile.mediaDevice = 'desktop';
-	} else {
-		Brandung.Mobile.mediaDevice = window.getComputedStyle(document.body, ':after').getPropertyValue('content');	
-	}
-};
-
-
-// add event listener
-$(window).load(function(){
-	Brandung.Mobile.getWindowWidth();
-	Brandung.Mobile.checkOrientation();
-});
-
-$(window).resize(function(){
-	// call function only on desktop devices
-	if((!Modernizr.touch && window.orientation != 0) || (Modernizr.touch && window.orientation != 0)) {
-		Brandung.Mobile.getWindowWidth();	
-	}
-});
-
-$(window).bind('orientationchange', function() {
-	Brandung.Mobile.checkOrientation();
-});
-
 /**
 #####################################
 #                                   #
-#      jQuery plugins object        #
+#    Load project dependencies and  #
+#    proceed                        #
 #                                   #
 #####################################
 */
-Brandung.Plugins = {
-	
-	init: function() {
-		// load plugin scripts
-		Brandung.Plugins.loadScripts();
-	}
-	
-};
+basket.require(
+    { 
+    	url : Brandung.folderPath + '/libs/vendor/jquery/jquery.min.js',
+    	unique : '1.11.0'
+    },
+    { 
+    	url : Brandung.folderPath + '/libs/vendor/modernizr/modernizr.custom.min.js',
+    	unique : '2.7.1'
+    }
+).then(function () {
+    basket.require(
+	    { 
+	    	url : Brandung.folderPath + '/libs/bra/loadmodule/jquery.loadmodule.min.js',
+	    	unique : '1.0.0'
+	    }
+	).then(function () {
+		/**
+		 #####################################
+		 #                                   #
+		 #    mobile stuff functions         #
+		 #                                   #
+		 #####################################
+		 */
+		Brandung.Mobile = {
 
-/**
- * load script
- *
- * If selector exist, call getScriptCall function to load external javascript source
- * {string} - URL to javascript
- * {function} - callback function
- */
-Brandung.Plugins.loadScripts = function() {
+			init : function() {
+			}
+		};
 
-	// dummy call
-	//	$('[SELECTOR]').loadModule([
-	//		Brandung.folderPath + '[PATH_TO_JS_SOURCE]'
-	//	], 
-	//	function () {
-	//		// do something after script has been loaded
-	//	});
-	
-};
+		/**
+		 * check orientation
+		 * 
+		 * add orientation class to body tag
+		 */
+		Brandung.Mobile.checkOrientation = function() {
 
-/**
- #####################################
- #                                   #
- #        helper functions           #
- #                                   #
- #####################################
- */
-Brandung.Helpers = {
+			if (window.orientation != 0) {
+				$('body').removeClass('portrait').addClass('landscape');
+			} else {
+				$('body').removeClass('landscape').addClass('portrait');
+			}
+			
+		};
 
-	init: function() {
-	}
+		/**
+		 * get media device selector
+		 */
+		if (Brandung.IE8) {
+			Brandung.Mobile.mediaDevice = 'desktop';
+		} else {
+			Brandung.Mobile.mediaDevice = window.getComputedStyle(document.body, ':after').getPropertyValue('content');	
+		}
 
-};
+		Brandung.Mobile.getWindowWidth = function() {
+			if (Brandung.IE8) {  // feature detection for IE <= IE8
+				Brandung.Mobile.mediaDevice = 'desktop';
+			} else {
+				Brandung.Mobile.mediaDevice = window.getComputedStyle(document.body, ':after').getPropertyValue('content');	
+			}
+		};
 
-/**
-#####################################
-#                                   #
-#        global functions           #
-#                                   #
-#####################################
-*/
-Brandung.Functions = {
-	
-	init: function() {
-	}
-	
-};
+		// add event listener
+		$(window).load(function(){
+			Brandung.Mobile.getWindowWidth();
+			Brandung.Mobile.checkOrientation();
+		});
 
+		$(window).resize(function(){
+			// call function only on desktop devices
+			if((!Modernizr.touch && window.orientation != 0) || (Modernizr.touch && window.orientation != 0)) {
+				Brandung.Mobile.getWindowWidth();	
+			}
+		});
 
+		$(window).bind('orientationchange', function() {
+			Brandung.Mobile.checkOrientation();
+		});
 
-// snippets placeholer
-// --- start|bra-pb: js ---
-// --- end|bra-pb: js ---
+		/**
+		#####################################
+		#                                   #
+		#      jQuery plugins object        #
+		#                                   #
+		#####################################
+		*/
+		Brandung.Plugins = {
+			
+			init: function() {
+				// load plugin scripts
+				Brandung.Plugins.loadScripts();
+			}
+			
+		};
 
+		/**
+		 * load scripts
+		 */
+		Brandung.Plugins.loadScripts = function() {
 
-/**
-#####################################
-#                                   #
-#         document ready            #
-#                                   #
-#####################################
-*/
-$(function(){
-	// init objects
-	Brandung.Mobile.init();
-	Brandung.Plugins.init();
-	Brandung.Helpers.init();
-	Brandung.Functions.init();
-});
+			/** Loading plugin
+			 *
+			 * @param {array} - List of dependency paths (js and or css)
+			 * @param {function} - callback function, when all dependencies are available
+			 * @param {string|int} - if this parameter changes, the dependencies will be refetched
+			 * from the server
+			 */
+			// dummy call
+			//	$('[SELECTOR]').loadModule([
+			//		Brandung.folderPath + '[PATH_TO_JS_SOURCE]'
+			//	], 
+			//	function () {
+			//		// do something after script has been loaded
+			//	}, 'unique');
+
+			// load hotfix files
+			$('body').loadModule([
+				Brandung.folderPath + '/hotfix.js',
+				'%%public%%/css/hotfix.css'
+			], function () {}, new Date().getTime());
+			
+		};
+
+		/**
+		 #####################################
+		 #                                   #
+		 #        helper functions           #
+		 #                                   #
+		 #####################################
+		 */
+		Brandung.Helpers = {
+
+			init: function() {
+			}
+
+		};
+
+		/**
+		#####################################
+		#                                   #
+		#        global functions           #
+		#                                   #
+		#####################################
+		*/
+		Brandung.Functions = {
+			
+			init: function() {
+			}
+			
+		};
+
+		// snippets placeholer
+		// --- start|bra-pb: js ---
+		// --- end|bra-pb: js ---
+
+		/**
+		#####################################
+		#                                   #
+		#         document ready            #
+		#                                   #
+		#####################################
+		*/
+		$(function(){
+			// init objects
+			Brandung.Mobile.init();
+			Brandung.Plugins.init();
+			Brandung.Helpers.init();
+			Brandung.Functions.init();
+		});
+	}, function(err) { console.error(err); });
+}, function(err) { console.error(err); });
